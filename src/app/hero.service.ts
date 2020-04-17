75e150eeca51020240cde5567824a74e9c63e9c1
juanwebmaster <darius.ball.dev.@hotmail.com>
2020-04-17 12:46:34 -0700

    Feat: CRUD added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,6 +9,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type:' : 'application/json'})
+  }
   constructor(
     private http: HttpClient,
     public messageService: MessageService
@@ -43,6 +46,30 @@ export class HeroService {
     .pipe(
       tap(_ => this.log(`fetch hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
+    )
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id=typeof hero === 'number' ? hero:hero.id;
+    const url=`${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_=>this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     )
   }
 }
